3e991db4c76d658647de14f0df78661fe85c1e20
Nadim <akramnadim064@gmail.com>
2025-12-28 21:24:33 +0530

    API

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,23 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+//   if (token) req.headers.Authorization = `Bearer ${token}`;
+//   return req;
+// });
+
+// export default API;
+
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
 });
 
 API.interceptors.request.use((req) => {
