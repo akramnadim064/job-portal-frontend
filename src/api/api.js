4d2cf43f24956df67dd63996066cfc562a48d46c
Nadim <akramnadim064@gmail.com>
2025-12-28 23:25:50 +0530

    new

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,23 @@
+// // import axios from "axios";
+
+// // const API = axios.create({
+// //   baseURL: "http://localhost:5000/api",
+// // });
+
+// // API.interceptors.request.use((req) => {
+// //   const token = localStorage.getItem("token");
+// //   if (token) req.headers.Authorization = `Bearer ${token}`;
+// //   return req;
+// // });
+
+// // export default API;
+
+
+
 // import axios from "axios";
 
 // const API = axios.create({
-//   baseURL: "http://localhost:5000/api",
+//   baseURL: `${process.env.REACT_APP_API_URL}/api`,
 // });
 
 // API.interceptors.request.use((req) => {
@@ -17,12 +33,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  baseURL: process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL}/api`
+    : "/api",
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
   return req;
 });
 
