025dceb73ad21696819b5b67ba623ef24b4370fc
Nadim <akramnadim064@gmail.com>
2025-12-29 15:18:49 +0530

    again

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,16 @@
-// // import axios from "axios";
+import axios from "axios";
 
-// // const API = axios.create({
-// //   baseURL: "http://localhost:5000/api",
-// // });
+const API = axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+});
 
-// // API.interceptors.request.use((req) => {
-// //   const token = localStorage.getItem("token");
-// //   if (token) req.headers.Authorization = `Bearer ${token}`;
-// //   return req;
-// // });
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
 
-// // export default API;
+export default API;
 
 
 
@@ -53,29 +53,30 @@
 
 
 
-import axios from "axios";
+// import axios from "axios";
 
-// Helper to determine the correct Base URL
-const getBaseURL = () => {
-  if (process.env.REACT_APP_API_URL) {
-    // If the Vercel env variable is set, use it
-    return `${process.env.REACT_APP_API_URL}/api`;
-  }
-  // Fallback for local development
-  return "http://localhost:5000/api";
-};
+// // Helper to determine the correct Base URL
+// const getBaseURL = () => {
+//   // if (process.env.REACT_APP_API_URL) {
+//   //   // If the Vercel env variable is set, use it
+//   //   return `${process.env.REACT_APP_API_URL}/api`;
+  
+//   // }
+//   // Fallback for local development
+//   return "http://localhost:5000";
+// };
 
-const API = axios.create({
-  baseURL: getBaseURL(),
-});
+// const API = axios.create({
+//   baseURL: getBaseURL(),
+// });
 
-// Attach Token to every request automatically
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
-});
+// // Attach Token to every request automatically
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return req;
+// });
 
-export default API;
+// export default API;
